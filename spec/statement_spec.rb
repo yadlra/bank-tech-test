b86d8eec5c3b1c9c22205cacfd7aad011c699a9d
yadlra <yadi.sb1@gmail.com>
2019-06-04 15:18:02 +0100

    Add withdraw transactions to statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,6 +3,7 @@ require 'statement'
 describe Statement do
   let(:statement) { described_class.new }
   let(:deposit_transaction) { { credit: 0, debit: 1000, balance: 1000 } }
+  let(:withdraw_transaction) { { credit: 1000, debit: 0, balance: 0 }}
   let(:date) { Date.parse('04/06/2019') }
 
 
@@ -13,9 +14,14 @@ describe Statement do
   end
 
   describe '#transaction_logging' do
-    it 'logs transactions to statement' do
+    it 'logs deposit transaction to statement' do
       statement.transaction_logging(deposit_transaction)
       expect(statement.transactions).to include {deposit_transaction}
+    end
+
+    it 'logs withdraw transaction to statement' do
+      statement.transaction_logging(withdraw_transaction)
+      expect(statement.transactions).to include {withdraw_transaction}
     end
   end
 end
